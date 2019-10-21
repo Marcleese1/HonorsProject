2b9ceab1808eb35cf58614e83a0253f0b54cf2de
Marc <30148234@cityofglacol.ac.uk>
2019-10-21 22:29:44 +0100

    removed old solitaire file

--- honors project/solitaire.py
+++ honors project/solitaire.py
@@ -11,7 +11,7 @@ import random
 from tkinter import *
 from Canvas import Rectangle, CanvasText, Group, Window
 
-
+#f
 # Fix a bug in Canvas.Group as distributed in Python 1.4.  The
 # distributed bind() method is broken.  Rather than asking you to fix
 # the source, we fix it here by deriving a subclass:
